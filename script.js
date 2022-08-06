0f6c297c190420eb623128fe04735c8a66456308
Laur <laur_laurentiu2013@yahoo.com>
2022-08-06 10:22:15 +0300

    create new div on each button click

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let myLibrary = [];
 
-function Book(title, author, pages){
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
-function addNewBooks(){
-    
+function addNewBooks() {
+    const addBtn = document.querySelector('.add');
+    const bookDiv = document.createElement('div');
+    const bookCards = document.querySelector('.book-cards');
+    addBtn.addEventListener('click', function () {
+        bookDiv.classList.add('card');
+        bookCards.insertAdjacentElement('beforebegin', bookDiv);
+        bookCards.appendChild(bookDiv);
+    }
+    )
 }
+
+addNewBooks();
